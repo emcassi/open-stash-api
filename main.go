0995e553c6c853856864b809e7a83c179e28a19f
Alex Wayne <alex.wayne.dev@gmail.com>
2024-11-30 02:36:47 -0700

    Set up middleware and base routing

--- main.go
+++ main.go
@@ -6,14 +6,20 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/emcassi/open-stash-api/routers"
 	"github.com/emcassi/open-stash-api/app"
+	"github.com/emcassi/open-stash-api/routers"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	r := chi.NewRouter()
+
+  r.Use(middleware.RequestID)
+  r.Use(middleware.RealIP)
+  r.Use(middleware.Logger)
+  r.Use(middleware.Recoverer)
 
 	envFile := "dev.env"
 	err := godotenv.Load(envFile)
@@ -28,5 +34,6 @@ func main() {
 
 	routers.HandleRoutes(r)
 	
-	http.ListenAndServe(fmt.Sprintf(":%s", port), r)
+	log.Printf("Listening at: http://127.0.0.1:%s\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
 }
